c67da936e95224d4ea064e3cce8bd30823bfb46d
trevor <scherling.trevor@gmail.com>
2015-08-07 00:12:56 -0700

    add intergration folder

--- spec/coin_machine_spec.rb
+++ spec/coin_machine_spec.rb
@@ -7,4 +7,16 @@ describe('Fixnum#coin_machine') do
     expect((3).coin_machine()).to(eq([0,0,0,3]))
   end
 
+  it('returns pennies and nickels for an amount greater than 5 and less than 10') do
+    expect((9).coin_machine()).to(eq([0,0,1,4]))
+  end
+
+  it ('returns pennies and nickels and dimes for an amount greater than 10 and less than 25') do
+    expect((17).coin_machine()).to(eq([0,1,1,2]))
+  end
+
+  it('returns the correct quantity of pennies, nickels, dimes and quarters for amount greater than 25 and less than 100') do
+    expect((41).coin_machine()).to(eq([1,1,1,1]))
+  end
+
 end
